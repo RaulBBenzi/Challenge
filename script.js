70c1d70f6a87f9c36c045026b8d91cb0f2b63616
RaulBBenzi <raulbiscaro@gmail.com>
2024-03-20 23:16:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ const botaoCopiar = document.querySelector(".botao-copiar");
 const textoVazio = document.querySelector(".texto-vazio");
 
 
-//A letra "e" é convertida para "enter"
-//A letra "i" é convertida para "imes"
-//A letra "a" é convertida para "ai"
-//A letra "o" é convertida para "ober"
-//A letra "u" é convertida para "ufat"
 
 let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
 
